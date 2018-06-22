a7790298e152ba6c5ed4a3bf564b90e4bf27e8ca
Dallin Abendroth <dallin.abendroth@utah.edu>
2018-06-21 21:43:43 -0600

    configured cabal to build executable

--- EnumRandom.hs
+++ EnumRandom.hs
@@ -3,7 +3,6 @@
 module EnumRandom where
 
 import System.Random
-import Test.QuickCheck
 
 newtype Enum' a = Enum' a
   deriving (Bounded, Enum, Show)
@@ -15,9 +14,6 @@ enumRandomR  (lo,hi) gen =
 enumRandom  :: (RandomGen g, Enum e, Bounded e) => g -> (e, g)
 enumRandom gen = 
     let (e, gen') = enumRandomR (minBound, maxBound) gen in (e, gen')
-
-instance (Enum a, Bounded a) => Arbitrary (Enum' a) where
-  arbitrary = choose (minBound, maxBound)
 
 instance (Enum a, Bounded a) => Random (Enum' a) where
   random = enumRandom
